800f9ad57349f6a679d342ce5dbf9e3de7bec19c
Serey <serey.ok20@gmail.com>
2025-07-03 14:48:33 +0700

    Initial commit

---
+++ app/app.js
@@ -0,0 +1,16 @@
+// app.js
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello, GitHub Actions!');
+});
+
+if (require.main === module) {
+  app.listen(port, () => {
+    console.log(`App listening at http://localhost:${port}`);
+  });
+}
+
+module.exports = app;
---
+++ test/test.js
@@ -0,0 +1,19 @@
+// test/test.js
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const app = require('../app');
+
+chai.use(chaiHttp);
+const expect = chai.expect;
+
+describe('GET /', function () {
+  it('should return hello message', function (done) {
+    chai.request(app)
+      .get('/')
+      .end(function (err, res) {
+        expect(res).to.have.status(200);
+        expect(res.text).to.equal('Hello, GitHub Actions!');
+        done();
+      });
+  });
+});
